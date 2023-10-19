65aa21a3c4edb4e0c0c76b44075e8f7941e0f9dd
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-10-19 15:59:24 -0400

    find and output: total rev, average rev, 1M stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+grand_total_rev =  Store.sum(:annual_revenue)
+average_rev = Store.average(:annual_revenue).round
+mill_stores = Store.where("annual_revenue >= ?", 1000000).count
+
+
+puts "The company's total revenue is #{grand_total_rev}"
+puts "The average store revenue is #{average_rev}"
+puts "There are #{mill_stores} stores that have 1M+ in revenue"

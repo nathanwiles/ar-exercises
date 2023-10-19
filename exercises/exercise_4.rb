ecd21213699322004ed386d9f4abd4acb55d099b
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-10-19 15:41:35 -0400

    modularize printing the names and revenue.

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,17 +7,26 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+def print_list(store_list)
+  store_list.each do |store|
+    puts "#{store.name} has an annual revenue of: $#{store.annual_revenue}"
+    end
+
+end
+
 Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
 Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
 @mens_stores = Store.where(mens_apparel: true)
 
-@mens_stores.each do |store|
-puts "#{store.name} has an annual revenue of: $#{store.annual_revenue}"
-end
+print_list(@mens_stores)
+
 
 @womens_apparel = Store.where(womens_apparel: true)
 @womens_apparel = @womens_apparel.filter do |store|
 store.annual_revenue < 1000000
 end
+
+print_list(@womens_apparel)

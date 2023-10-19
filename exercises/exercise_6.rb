243369652df65033c314fe3f37a80ef4df4d3064
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-10-19 16:31:28 -0400

    add associations, add employees.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,22 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Vicky", last_name: "Kuthri", hourly_rate: 60)
+@store1.employees.create(first_name: "Jared", last_name: "Luther", hourly_rate: 60)
+@store1.employees.create(first_name: "Terrence", last_name: "King", hourly_rate: 60)
+@store1.employees.create(first_name: "Jessie", last_name: "Putin", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Karl", last_name: "Keller", hourly_rate: 60)
+@store2.employees.create(first_name: "Luke", last_name: "Skywalker", hourly_rate: 60)
+@store2.employees.create(first_name: "Jeryl", last_name: "Patel", hourly_rate: 60)
+@store2.employees.create(first_name: "Ken", last_name: "Lathier", hourly_rate: 60)
+@store2.employees.create(first_name: "Jen", last_name: "Duffy", hourly_rate: 60)
